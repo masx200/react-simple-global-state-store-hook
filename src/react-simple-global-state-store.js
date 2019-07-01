fea7e7eca2c247f6551c81f2c86e913afda8f7ab
masx200 <34191203+masx200@users.noreply.github.com>
2019-07-01 16:51:19 +0800

    首次提交

---
+++ src/react-simple-global-state-store.js
@@ -0,0 +1,101 @@
+import { useState, useEffect } from "react";
+const reactsimpleglobalstatestore = {};
+
+function newobjjson(obj) {
+  //   if (typeof obj !== "object") {
+  //     throw new Error("传入的参数必须是个object!");
+  //   }
+  return JSON.parse(JSON.stringify(obj));
+}
+export default function(jsonobject) {
+  //   if (typeof jsonobject !== "object") {
+  //     throw new Error("传入的参数必须是个object!");
+  //   } else {
+  const newjsonobj = newobjjson(jsonobject);
+  //   console.log("输入值", newjsonobj);
+  var newobjtoreturn = {};
+  Object.keys(newjsonobj).forEach(key => {
+    let [state, setstate] = useState(newjsonobj[key]);
+    if ("undefined" === typeof reactsimpleglobalstatestore[key]) {
+      reactsimpleglobalstatestore[key] = newjsonobj[key];
+    }
+    // window.dispatchEvent("globalstatechange");
+    const eventhandler = () => {
+      var newstate = reactsimpleglobalstatestore[key];
+      if (newstate !== state) {
+        // console.log(state, newstate);
+        console.log("接受事件" + "globalstatechange");
+        //   console.log(e);
+
+        // console.log("全局状态改变", reactsimpleglobalstatestore);
+
+        setstate(newstate);
+      }
+    };
+    useEffect(() => {
+      window.addEventListener("globalstatechange", eventhandler);
+      return () => {
+        window.removeEventListener("globalstatechange", eventhandler);
+      };
+    }, []);
+
+    newobjtoreturn[key] = [
+      state,
+
+      newstate => {
+        if (newstate !== state) {
+          //   console.log(state, newstate);
+          reactsimpleglobalstatestore[key] = newstate;
+          console.log("触发事件" + "globalstatechange");
+          console.log("全局状态改变", reactsimpleglobalstatestore);
+          window.dispatchEvent(new Event("globalstatechange"));
+        }
+      }
+    ];
+  });
+  //   console.log("输出值", newobjtoreturn);
+  return newobjtoreturn;
+  //   }
+}
+
+// function(jsonobject) {
+//   if (typeof jsonobject !== "object") {
+//     throw new Error("传入的参数必须是个object!");
+//     // return;
+//   } else {
+//     var newobjtoreturn = {};
+//     const newjsonobj = newobjjson(jsonobject);
+//     console.log("输入值", newjsonobj);
+//     Object.keys(newjsonobj).forEach(key => {
+//       //   console.log(key, newjsonobj[key]);
+//       if ("undefined" === typeof reactsimpleglobalstatestore[key]) {
+//         var arr = useState(newjsonobj[key]);
+//         reactsimpleglobalstatestore[key] = [];
+//         reactsimpleglobalstatestore[key][0] = arr[0];
+//         reactsimpleglobalstatestore[key][1] = newstatevalue => {
+//           reactsimpleglobalstatestore[key][0] = newstatevalue;
+//           console.log("全局状态改变", reactsimpleglobalstatestore);
+//           reactsimpleglobalstatestore[key][2](newstatevalue);
+//         };
+//         reactsimpleglobalstatestore[key][2] = arr[1];
+
+//         // newstatevalue => {
+//         //   reactsimpleglobalstatestore[key][0] = newstatevalue;
+//         //   arr[1](newstatevalue);
+//         // };
+//       } else {
+//         console.warn(
+//           "全局状态已经存在",
+//           key,
+//           reactsimpleglobalstatestore[key][0]
+//         );
+//       }
+//       newobjtoreturn[key] = reactsimpleglobalstatestore[key];
+//     });
+
+//     console.log("输出值", newobjtoreturn);
+//     console.log("全局状态合集", reactsimpleglobalstatestore);
+//     /* 返回值包含函数不能转成新的json! */
+//     return newobjtoreturn;
+//   }
+// }
