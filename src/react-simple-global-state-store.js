9af57836647418c24f998449a11d15c3cc9f0680
masx200 <34191203+masx200@users.noreply.github.com>
2019-08-19 19:23:12 +0800

    Update react-simple-global-state-store.js

--- src/react-simple-global-state-store.js
+++ src/react-simple-global-state-store.js
@@ -1,5 +1,10 @@
 "use strict"
 
+export function getGlobalStates(){
+
+return newobjjson(simpleglobalstatestore)
+
+}
 import { useState, useEffect, useCallback } from "react";
 
 function jsondeepequal(a, b) {
