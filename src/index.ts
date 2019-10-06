66347a00b5e5ee8b2982764cdf68c33db1ba3f44
masx200 <34191203+masx200@users.noreply.github.com>
2019-10-06 08:20:47 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,127 +1,114 @@
-export function useGlobalStore(name){
- return useGlobalStoreold({[name]:undefined})[name]
+export function useGlobalStore(name) {
+    return useGlobalStoreold({ [name]: undefined })[name];
 }
-function isfunction(a){return "function"===typeof a}
-
-export function changeState(keyname,newvalue){
-
-const key=keyname
-let newstate=newvalue
-
-
-const oldstate=simpleglobalstatestore[key]
-const state=oldstate
-if(isfunction(newstate)){
-newstate=newstate.call(undefined,state)
+function isfunction(a) {
+    return 'function' === typeof a;
 }
-if (isinvalidstate(newstate)) {
-          throw Error("invalid state");
-        }
-        if (!jsondeepequal(newstate, state)) {
-          simpleglobalstatestore[key] = jsonparsestringify(newstate);
-          console.log("全局状态改变", simpleglobalstatestore);
-          temptarget.dispatchEvent(new Event(eventname));
-        }
-
+export function changeState(keyname, newvalue) {
+    const key = keyname;
+    let newstate = newvalue;
+    const oldstate = simpleglobalstatestore[key];
+    const state = oldstate;
+    if (isfunction(newstate)) {
+        newstate = newstate.call(undefined, state);
+    }
+    if (isinvalidstate(newstate)) {
+        throw Error('invalid state');
+    }
+    if (!jsondeepequal(newstate, state)) {
+        simpleglobalstatestore[key] = jsonparsestringify(newstate);
+        console.log('全局状态改变', simpleglobalstatestore);
+        temptarget.dispatchEvent(new Event(eventname));
+    }
 }
-"use strict";
+'use strict';
 function jsonparsestringify(o) {
-  return JSON.parse(JSON.stringify(o));
+    return JSON.parse(JSON.stringify(o));
 }
 export function getGlobalStates() {
-  return newobjjson(simpleglobalstatestore);
+    return newobjjson(simpleglobalstatestore);
 }
-import { useState, useEffect, useCallback } from "react";
+import {
+    useState,
+    useEffect,
+    useCallback
+} from 'react';
 function jsondeepequal(a, b) {
-  return JSON.stringify(a) === JSON.stringify(b);
+    return JSON.stringify(a) === JSON.stringify(b);
 }
 function isinvalidstate(newstate) {
-  return (
-    "undefined" === typeof newstate ||
-    "function" === typeof newstate ||
-    newstate === null ||
-    "symbol" === typeof newstate
-  );
+    return 'undefined' === typeof newstate || 'function' === typeof newstate || newstate === null || 'symbol' === typeof newstate;
 }
 const temptarget = new EventTarget();
 const simpleglobalstatestore = {};
 function newobjjson(obj) {
-  if (typeof obj !== "object") {
-    throw new TypeError("传入的参数必须是个object!");
-  }
-  return jsonparsestringify(obj);
+    if (typeof obj !== 'object') {
+        throw new TypeError('传入的参数必须是个object!');
+    }
+    return jsonparsestringify(obj);
 }
 function isplainobject(o) {
-  return (
-    typeof o === "object" &&
-    Object.prototype.toString.call(o) === "[object Object]"
-/* &&
-    o.__proto__ === Object.prototype*/
-  );
+    return typeof o === 'object' && Object.prototype.toString.call(o) === '[object Object]';
 }
-
 function useGlobalStoreold(jsonobject) {
-  if (!isplainobject(jsonobject)) {
-    throw Error("invalid object");
-  }
-  const newjsonobj = newobjjson(jsonobject);
-  const newobjtoreturn = {};
-  Object.keys(newjsonobj).forEach(key => {
-    const eventname = key;
-    if ("undefined" === typeof simpleglobalstatestore[key]) {
-      simpleglobalstatestore[key] = newjsonobj[key];
+    if (!isplainobject(jsonobject)) {
+        throw Error('invalid object');
     }
-    const initialstate =
-      "undefined" !== typeof simpleglobalstatestore[key]
-        ? simpleglobalstatestore[key]
-        : newjsonobj[key];
-    if (isinvalidstate(initialstate)) {
-      throw Error("invalid state");
-    }
-    const [state, setstate] = useState(jsonparsestringify(initialstate));
-    const eventhandler = useCallback(() => {
-      const newstate = simpleglobalstatestore[key];
-      if (!jsondeepequal(newstate, state))
-        setstate(jsonparsestringify(newstate));
-    }, []);
-    useEffect(() => {
-      temptarget.addEventListener(eventname, eventhandler);
-      temptarget.dispatchEvent(new Event(eventname));
-      return () => {
-        temptarget.removeEventListener(eventname, eventhandler);
-      };
-    }, []);
-    newobjtoreturn[key] = [
-      state,
-      newstate => {
-if(isfunction(newstate)){
-newstate=newstate.call(undefined,state)
-}
-        if (isinvalidstate(newstate)) {
-          throw Error("invalid state");
+    const newjsonobj = newobjjson(jsonobject);
+    const newobjtoreturn = {};
+    Object.keys(newjsonobj).forEach(key => {
+        const eventname = key;
+        if ('undefined' === typeof simpleglobalstatestore[key]) {
+            simpleglobalstatestore[key] = newjsonobj[key];
         }
-        if (!jsondeepequal(newstate, state)) {
-          simpleglobalstatestore[key] = jsonparsestringify(newstate);
-          console.log("全局状态改变", simpleglobalstatestore);
-          temptarget.dispatchEvent(new Event(eventname));
+        const initialstate = 'undefined' !== typeof simpleglobalstatestore[key] ? simpleglobalstatestore[key] : newjsonobj[key];
+        if (isinvalidstate(initialstate)) {
+            throw Error('invalid state');
         }
-      }
-    ];
-  });
-  return newobjtoreturn;
+        const [state, setstate] = useState(jsonparsestringify(initialstate));
+        const eventhandler = useCallback(() => {
+            const newstate = simpleglobalstatestore[key];
+            if (!jsondeepequal(newstate, state))
+                setstate(jsonparsestringify(newstate));
+        }, []);
+        useEffect(() => {
+            temptarget.addEventListener(eventname, eventhandler);
+            temptarget.dispatchEvent(new Event(eventname));
+            return () => {
+                temptarget.removeEventListener(eventname, eventhandler);
+            };
+        }, []);
+        newobjtoreturn[key] = [
+            state,
+            newstate => {
+                if (isfunction(newstate)) {
+                    newstate = newstate.call(undefined, state);
+                }
+                if (isinvalidstate(newstate)) {
+                    throw Error('invalid state');
+                }
+                if (!jsondeepequal(newstate, state)) {
+                    simpleglobalstatestore[key] = jsonparsestringify(newstate);
+                    console.log('全局状态改变', simpleglobalstatestore);
+                    temptarget.dispatchEvent(new Event(eventname));
+                }
+            }
+        ];
+    });
+    return newobjtoreturn;
 }
 export function initGlobalState(jsonobject) {
-  if (!isplainobject(jsonobject)) {
-    throw Error("invalid object");
-  }
-  const newjsonobj = newobjjson(jsonobject);
-  const newobjtoreturn = {};
-  Object.keys(newjsonobj).forEach(key => {
-    if ("undefined" === typeof simpleglobalstatestore[key]) {
-      simpleglobalstatestore[key] = newjsonobj[key];
+    if (!isplainobject(jsonobject)) {
+        throw Error('invalid object');
     }
-    newobjtoreturn[key] = simpleglobalstatestore[key];
-  });
-  console.log("全局状态生成", simpleglobalstatestore);
-  return newobjtoreturn;
+    const newjsonobj = newobjjson(jsonobject);
+    const newobjtoreturn = {};
+    Object.keys(newjsonobj).forEach(key => {
+        if ('undefined' === typeof simpleglobalstatestore[key]) {
+            simpleglobalstatestore[key] = newjsonobj[key];
+        }
+        newobjtoreturn[key] = simpleglobalstatestore[key];
+    });
+    console.log('全局状态生成', simpleglobalstatestore);
+    return newobjtoreturn;
 }
